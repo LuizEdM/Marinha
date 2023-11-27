fcd153e638e125d5a64dc0f95f9e708440c34ea4
duudubr <duudubr@gmail.com>
2023-11-27 08:25:55 -0300

    última

--- trabalhouc12/page1.js
+++ trabalhouc12/page1.js
@@ -6,14 +6,22 @@ $(document).ready(function(){
         arrows: true 
     });
 });
+
 $(document).ready(function() {
+    function showSection(target) {
+        $('.content').hide(); 
+        $('#' + target).show(); 
+    }
+
     $('.menu-link').on('click', function(e) {
         e.preventDefault();
         var target = $(this).data('target');
-        $('.content').hide();
-        $('#' + target).fadeIn(); 
+        showSection(target);
     });
+
+    showSection('home');
 });
+
 function validateForm() {
     var cpfPattern = /\d{3}\.\d{3}\.\d{3}-\d{2}/;
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -23,17 +31,14 @@ function validateForm() {
     var cpfError = document.getElementById('cpfError');
     var emailError = document.getElementById('emailError');
 
-   
     cpfError.textContent = '';
     emailError.textContent = '';
 
-   
     if (!cpfPattern.test(cpfInput.value)) {
         cpfError.textContent = 'CPF inválido';
         return;
     }
 
-    
     if (!emailPattern.test(emailInput.value)) {
         emailError.textContent = 'Email inválido';
         return;
@@ -43,3 +48,28 @@ function validateForm() {
 }
 
 
+
+$('#cpf').on('input', function() {
+    var inputValue = $(this).val();
+    var formattedValue = inputValue.replace(/\D/g, ''); 
+    if (formattedValue.length <= 11) {
+        formattedValue = formattedValue.replace(/(\d{3})(\d{3})(\d{3})/, '$1.$2.$3');
+    } else {
+        formattedValue = formattedValue.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+    }
+
+    formattedValue = formattedValue.replace(/(\d{2})$/, '-$1');
+
+    $(this).val(formattedValue);
+});
+
+$('#numero').on('input', function() {
+    var inputValue = $(this).val();
+    var formattedValue = inputValue.replace(/\D/g, '');
+    if (formattedValue.length >= 11) {
+        formattedValue = formattedValue.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+    } else {
+        formattedValue = formattedValue.replace(/(\d{2})(\d{4,5})(\d{4})/, '($1) $2-$3');
+    }
+    $(this).val(formattedValue);
+});
